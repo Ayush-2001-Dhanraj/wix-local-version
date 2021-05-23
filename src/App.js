4b0b7081da5ef4cafba588bd3bbb51e242cc44e9
Ayush Dhanraj <dhanrajaayush123@gmail.com>
2021-05-23 15:35:53 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Components/Header"
+// import Contact from "./Components/Contact";
+import Footer from "./Components/Footer"
+import Resume from "./Components/Resume"
+
+function App() {
+
+  return (<div>
+    <Header/> {/* <Contact/> */}
+    <Resume/>
+    <Footer/>
+  </div>);
+}
+
+export default App;
